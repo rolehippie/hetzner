23ebea02001ce32539c4e51d4d1f8b9aedde3cdd
Thomas Boerger <thomas@webhippie.de>
2020-02-21 01:46:27 +0100

    Restructure including drone and molecule tests

---
+++ HEADER.md
@@ -0,0 +1,3 @@
+# hetzner
+
+[![Build Status](https://cloud.drone.io/api/badges/rolehippie/hetzner/status.svg)](https://cloud.drone.io/rolehippie/hetzner)
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,13 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_apt_sources_file(host):
+    file = host.file("/etc/apt/sources.list.d/hetzner-mirror.list")
+    assert file.exists
+    assert file.contains("http://mirror.hetzner.de/ubuntu/packages")
