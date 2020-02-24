c48e1fbcfa8136df96cfa4e1f4da1b3c4ef8e156
Thomas Boerger <thomas@webhippie.de>
2020-02-24 10:23:55 +0100

    Add more common test naming

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,7 +7,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_apt_sources_file(host):
+def test_sources_file(host):
     file = host.file("/etc/apt/sources.list.d/hetzner-mirror.list")
     assert file.exists
     assert file.contains("http://mirror.hetzner.de/ubuntu/packages")
